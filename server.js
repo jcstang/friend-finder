440bef000bc4574363520104d865a4f9bf019d6e
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-20 12:27:45 -0600

    router setup for htmlroutes

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const path = require('path');
 const chalk = require('chalk');
+const htmlRouter = require('./app/routing/htmlRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 8085;
 
-app.get('/', (req, res) => {
-  res.send('hi');
-});
+app.use('/', htmlRouter);
+
+// app.get('/', (req, res) => {
+//   res.send('hi');
+// });
 
 app.listen(PORT, () => {
   console.log(`listening on port ${chalk.cyanBright(PORT)}`);
