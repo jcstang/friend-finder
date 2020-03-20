8f634ea65243e79089afc70437d20e8dba7fb2e2
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-20 14:06:21 -0600

    now serving survey

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -10,14 +10,15 @@ router.use(function timeLog (req, res, next) {
 
 // define the home page route
 router.get('/', function (req, res) {
-  // res.send('home page');
   // res.sendFile(path.join(__dirname, '../public','home.html'));
   res.sendFile(path.join(__dirname, '../', 'public','home.html'));
 });
 
 // define the about route
 router.get('/survey', function (req, res) {
-  res.send('survey page')
+  // res.send('survey page')
+  // res.sendFile(path.join(__dirname, 'survey.html'));
+  res.sendFile(path.join(__dirname, '../public', 'survey.html'));
 });
 
 module.exports = router;
