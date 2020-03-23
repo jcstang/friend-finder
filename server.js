2f7f5f2a1648d09dc16cfc145574bc0a361f96b7
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-23 11:52:41 -0600

    handlebars for each working. movied routes to server.js

--- server.js
+++ server.js
@@ -4,13 +4,27 @@ const chalk = require('chalk');
 const htmlRouter = require('./app/routing/htmlRoutes');
 const apiRouter = require('./app/routing/apiRoutes');
 const friends = require('./app/data/friends');
+const expressHandlebars = require('express-handlebars');
+const questions = require('./app/data/questions');
 
 const app = express();
-const PORT = process.env.PORT || 8085;
-
+const PORT = process.env.PORT || 8085
 app.use('/api', apiRouter);
-app.use('/', htmlRouter);
+// app.use('/', htmlRouter);
 app.use(express.static('public'));
+app.use(express.static('public/views'));
+
+app.engine("handlebars", expressHandlebars({ defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+app.get('/', function(req, res) {
+  // res.sendFile(path.join(__dirname, '.app', 'public', 'home.html'));
+  res.render('home');
+});
+
+app.get('/survey', function(req, res) {
+  res.render('survey', questions);
+});
 
 // friends.forEach(element => {
 //   console.log(element.scores);
