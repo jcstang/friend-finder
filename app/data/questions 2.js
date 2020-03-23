59d85faf8136a8b82dd7997391a5a247f2aac78a
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-23 11:23:24 -0600

    stuff

---
+++ app/data/questions 2.js
@@ -0,0 +1,48 @@
+const questions = [
+  {
+    number: 1,
+    content: "Your mind is always buzzing with unexplored ideas and plans."
+  },
+  {
+    number: 2,
+    content: "Generally speaking, you rely more on your experience than your imagination."
+  },
+  {
+    number: 3,
+    content: "You find it easy to stay relaxed and focused even when there is some pressure."
+  },
+  {
+    number: 4,
+    content: "You rarely do something just out of sheer curiosity."
+  },
+  {
+    number: 5,
+    content: "People can rarely upset you."
+  },
+  {
+    number: 6,
+    content: "It is often difficult for you to relate to other people’s feelings."
+  },
+  {
+    number: 7,
+    content: "In a discussion, truth should be more important than people’s sensitivities."
+  },
+  {
+    number: 8,
+    content: "You rarely get carried away by fantasies and ideas."
+  },
+  {
+    number: 9,
+    content: "You think that everyone’s views should be respected regardless of whether they are supported by facts or not."
+  },
+  {
+    number: 10,
+    content: "You feel more energetic after spending time with a group of people."
+  }
+
+];
+
+
+module.exports = {
+  questions: questions
+}
