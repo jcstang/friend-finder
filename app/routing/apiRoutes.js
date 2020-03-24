66a2307c4ebd34fd1dea3161dfee5f72f33043b4
Jacob Stanger <jacobcstanger@gmail.com>
2020-03-24 15:29:42 -0600

    lots of cleanup of comments and unused funcs

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const path = require('path');
-const friends = require('../data/friends');
+// const path = require('path');
+// const friends = require('../data/friends');
 const router = express.Router();
-const fileHandler = require('fs');
+// const fileHandler = require('fs');
 
-let friendsArr = [
-  {
-    name: 'billy',
-    photo: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg',
-    scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-  }
-];
+// let friendsArr = [
+//   {
+//     name: 'billy',
+//     photo: 'https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg',
+//     scores: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//   }
+// ];
 
 let friendArrObject = [
   {
@@ -31,106 +31,62 @@ router.use(function timeLog(req, res, next) {
 });
 
 router.get('/friends', (req, res) => {
-  // getFriendsDB();
-  // res.sendFile(path.join(__dirname, '../', 'data', 'friends.json'));
-  // res.json(getLocalData());
   res.json(friendArrObject);
 });
 
 router.post('/friends', (req, res) => {
-  // TODO: get survey data
-  
-
-  // TODO: push new friend to db
-  // console.log(req);
-  
-  // console.log("here is req.body");
-  // console.log(req.body);
-  // updateFriendsDB(req.body);
-  // updateLocalData(JSON.parse(req.body));
-
   const scoreArr = req.body.scores.map(function(score) {
-    // console.log('score inside map');
-    // console.log(score);
     return parseInt(score);
   });
 
-  // console.log('here is req.body before score change');
-  // console.log(req.body.scores);
-  
-  // score array of ints now added back to the object. 
+  // assigned int value scores to orginal object
   req.body.scores = scoreArr;
 
-  // console.log('here is req.body after score change');
-  // console.log(req.body.scores);
-  
-
-  // updateLocalData(req.body);
   friendArrObject.push(req.body);
-  // console.log(friendsArr);
-
-  
-
-  // fileHandler.appendFile(path.join(__dirname, "../", "data", "friends.json"), req.body, function(error) {
-  //   if (error) {
-  //     console.log(error);
-  //   } else {
-  //     console.log('hooray!');
-      
-  //   }
-  // });
-  
 
   res.end('posted!!!');
-  
 });
 
-// function convertScoresToInts(arr) {
-//   const scoreArr = arr.map(function(score) {
-//     return parseInt(score);
-//   });
+
+
+// function updateLocalData(value) {
+//   friendArrObject.push(value);
 // }
 
-
-// just JS object
-// =================================================
-function updateLocalData(value) {
-  friendArrObject.push(value);
-}
-
-function getLocalData() {
-  return friendArrObject;
-}
+// function getLocalData() {
+//   return friendArrObject;
+// }
 
 
 // FS - file handler
 // =================================================
-function updateFriendsDB(value) {
+// function updateFriendsDB(value) {
 
-  friendsArr.push(value);
+//   friendsArr.push(value);
 
-  fileHandler.writeFile(path.join(__dirname, "../", "data", "friends.json"), JSON.stringify(friendsArr), function(err) {
-    if(err) {
-      console.log(err);
-    } else {
-      console.log('hooray!!');
-    }
-  });
-}
+//   fileHandler.writeFile(path.join(__dirname, "../", "data", "friends.json"), JSON.stringify(friendsArr), function(err) {
+//     if(err) {
+//       console.log(err);
+//     } else {
+//       console.log('hooray!!');
+//     }
+//   });
+// }
 
-function getFriendsDB() {
-  fileHandler.readFile(path.join(__dirname, "../", "data", "friends.json"), {encoding: 'utf8'}, function(err, data) {
-    console.log('here is data, getFriendDB');
-    console.log(data);
+// function getFriendsDB() {
+//   fileHandler.readFile(path.join(__dirname, "../", "data", "friends.json"), {encoding: 'utf8'}, function(err, data) {
+//     console.log('here is data, getFriendDB');
+//     console.log(data);
     
-    // FIXME: scores coming in as strings in the friends.json file instead of numbers
-    friendsArr = JSON.parse(data);
+//     // FIXME: scores coming in as strings in the friends.json file instead of numbers
+//     friendsArr = JSON.parse(data);
     
-    // friendsArr = data;
-  });
-}
+//     // friendsArr = data;
+//   });
+// }
 
 
 
-
+// EXPORT the router
+// =================================================
 module.exports = router;
